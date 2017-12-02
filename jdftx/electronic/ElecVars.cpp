9160f7d42069d771b7c86776e1b458c50703464d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-02 08:40:34 +0700

    Add mistakenly deleted original source of jdftx/electronic

--- jdftx/electronic/ElecVars.cpp
+++ jdftx/electronic/ElecVars.cpp
@@ -1,23 +1,3 @@
-/*-------------------------------------------------------------------
-Copyright 2011 Ravishankar Sundararaman, Kendra Letchworth Weaver, Deniz Gunceler
-Copyright 1996-2003 Sohrab Ismail-Beigi
-
-This file is part of JDFTx.
-
-JDFTx is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-JDFTx is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with JDFTx.  If not, see <http://www.gnu.org/licenses/>.
--------------------------------------------------------------------*/
-
 #include <electronic/Everything.h>
 #include <electronic/ElecMinimizer.h>
 #include <electronic/ColumnBundle.h>
