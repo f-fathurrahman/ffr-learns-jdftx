d5d85f979cc2d6b33beea3b55dbe06a65d5e8d9a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-10-23 15:21:37 +0700

    Add rpath to build script

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ FLAGS="-Wall -O0 -g -ftemplate-depth-512 -std=c++0x"
 INCLUDE="-I./jdftx"
 
 #LIBS="libjdftx.a -lgsl -lfftw3_threads -lfftw3 -lcblas -lopenblas -lpthread"
-LIBS="-L ./ -ljdftx -lgsl -lfftw3_threads -lfftw3 -lcblas -lopenblas -lpthread"
+#LIBS="-L ./ -ljdftx -lgsl -lfftw3_threads -lfftw3 -lcblas -lopenblas -lpthread"
+
+LIBS="-Wl,-rpath,/home/efefer/WORKS/my_github_repos/ffr-learns-jdftx: libjdftx.so -lgsl -lfftw3_threads -lfftw3 -lcblas -lopenblas -lpthread"
 
 mpic++ $INCLUDE $FLAGS $1 -o $prefix.x $LIBS
