72077fb066b14c5260e3b07a7cd8d879d5531595
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-23 14:34:08 +0700

    Test operators again

--- debug_operators.cpp
+++ debug_operators.cpp
@@ -32,11 +32,13 @@ void debug_operators( Everything* e )
     Oc = O(e->eVars.C[0]);
 
     double detR = Oc.basis->gInfo->detR;
-    cout << "detR   = " << endl;
+    cout << "detR   = " << detR << endl;
     cout << "1/detR = " << 1.0/detR << endl;
     cout << "1/sqrt(detR) = " << 1.0/sqrt(detR) << endl;
 
-    // Test dot
+    //e->eVars.orthonormalize(0);
+
+    // Test dot between different column of C[0]
     ColumnBundle c1, c2;
     c1 = e->eVars.C[0].getSub(1,2);
     c2 = e->eVars.C[0].getSub(2,3);
@@ -48,6 +50,20 @@ void debug_operators( Everything* e )
     
     cout << "dot evars.C[1] = " << dot(e->eVars.C[1], e->eVars.C[1])/e->eVars.C[1].nCols() << endl;
     cout << "dot Oc         = " << dot(Oc, Oc) << endl; 
+
+    c1 = e->eVars.C[0];
+    cout << "c1.nCols() = " << c1.nCols() << endl;
+    cout << "dot C0, C0 = " << dot(c1, c1)/c1.nCols() << endl;
+    cout << "dot C0, C0 = " << dot(c1, c1) << endl;
+
+    complex* data = c1.data();
+    double ss = 0.0;
+    for(size_t i = 0; i < c1.colLength(); i++) {
+        size_t idx = c1.index(0,i);
+        ss = ss + data[idx].x * data[idx].x + data[idx].y * data[idx].y;
+        //logPrintf("dat = [%le,%le]\n", data[idx].x, data[idx].y);
+    }
+    cout << "ss = " << 2*ss << endl;
 
     return;
 }
@@ -61,7 +77,7 @@ int main( int argc, char** argv )
     parse(readInputFile(ip.inputFilename), e, ip.printDefaults);
     e.setup();
 
-    debug_operator_O(&e);
+    //debug_operator_O(&e);
     
     debug_operators(&e);
 
