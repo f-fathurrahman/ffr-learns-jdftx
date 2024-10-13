11b55d5c4f7a3d47bddb48d4802492259d859c09
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-10-13 18:25:25 +0700

    Change tab size

--- debug_elecEnergyAndGrad.cpp
+++ debug_elecEnergyAndGrad.cpp
@@ -13,24 +13,24 @@
 
 int main( int argc, char** argv )
 {
-    Everything e;
-    InitParams ip("Performs Joint DFT calculations.", &e);
-    initSystemCmdline(argc, argv, ip);
-    parse(readInputFile(ip.inputFilename), e, ip.printDefaults);
-    e.setup();
+  Everything e;
+  InitParams ip("Performs Joint DFT calculations.", &e);
+  initSystemCmdline(argc, argv, ip);
+  parse(readInputFile(ip.inputFilename), e, ip.printDefaults);
+  e.setup();
 
-    MyElecGradient g, Kg;
-    
-    g.init(e);
-    Kg.init(e);
+  MyElecGradient g, Kg;
+  
+  g.init(e);
+  Kg.init(e);
 
-    double Etot = my_elecEnergyAndGrad( e, &g, &Kg, false ); // ener is included in e
+  double Etot = my_elecEnergyAndGrad( e, &g, &Kg, false ); // ener is included in e
 
-    logPrintf("Etot = %18.10f\n", Etot);
+  logPrintf("Etot = %18.10f\n", Etot);
 
-    printf("\n");
-    printf("%s is finished\n", argv[0]);
-    printf("\n");
+  printf("\n");
+  printf("%s is finished\n", argv[0]);
+  printf("\n");
 
-    return 0;
+  return 0;
 }
