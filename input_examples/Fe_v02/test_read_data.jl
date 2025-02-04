2539056dd09ca2e8ffc838a810fc30a932ce557d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-02-04 13:38:03 +0700

    Read more

--- input_examples/Fe_v02/test_read_data.jl
+++ input_examples/Fe_v02/test_read_data.jl
@@ -35,7 +35,7 @@ function load_Hsub(; filename="eVars_Hsub.dat")
     Nkspin = 8
     Nstates = 12
 
-    Ndata = sum(Nstates*Nstates * Nkspin)
+    Ndata = Nstates * Nstates * Nkspin
     raw_data = Vector{ComplexF64}(undef, Ndata)
 
     file = open(filename, "r")
@@ -50,11 +50,36 @@ function load_Hsub(; filename="eVars_Hsub.dat")
         Hsub[ikspin] = zeros(ComplexF64, Nstates, Nstates)
         Hsub[ikspin][:,:] = reshape(
             raw_data[idx_start:idx_stop], Nstates, Nstates
-        ) # need transpose
+        ) # need transpose?
         idx_start += Ndata_k
     end
     return Hsub
 end
+
+
+function load_diagMatrix(filename, T)
+    Nkspin = 8
+    Nstates = 12
+
+    Ndata = Nstates * Nkspin
+    raw_data = Vector{T}(undef, Ndata)
+
+    file = open(filename, "r")
+    read!(file, raw_data);
+    close(file)
+
+    D = Vector{Vector{T}}(undef, Nkspin)
+    idx_start = 1
+    for ikspin in 1:Nkspin
+        Ndata_k = Nstates
+        idx_stop = idx_start + Ndata_k - 1
+        D[ikspin] = zeros(T, Nstates)
+        D[ikspin][:] = raw_data[idx_start:idx_stop]
+        idx_start += Ndata_k
+    end
+    return D
+end
+
 
 using Serialization
 function main()
